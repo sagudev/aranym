2fd5d2ecd8aeb979050b9a77908a0fbd72bd84ac
sagudev <samo.golez@outlook.com>
2020-02-21 09:38:23 +0100

    locales

--- .travis/in_emu.sh
+++ .travis/in_emu.sh
@@ -7,20 +7,22 @@ uname -a
 apt update
 apt install sudo -y -qq
 cd "/home/travis/build/${TRAVIS_REPO_SLUG}"
-#cat > ~/.sbuildrc << __EOF__
-#$apt_allow_unauthenticated = 1;
+cat > ~/.sbuildrc << __EOF__
+$apt_allow_unauthenticated = 1;
 
 # Directory for writing build logs to
-#$log_dir=$ENV{HOME}."/ubuntu/logs";
+$log_dir=$ENV{HOME}."/ubuntu/logs";
 
 # don't remove this, Perl needs it:
-#1;
-#__EOF__
+1;
+__EOF__
 #echo "LC_ALL=en_US.UTF-8" >> /etc/environment
 #echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 #echo "LANG=en_US.UTF-8" > /etc/locale.conf
 #locale-gen en_US.UTF-8
-export LANG=C.UTF-8
+export LANG=en_US.utf8
+apt install -y locales
+localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 . ./.travis/install_prerequisities.sh
 . ./.travis/setup_env.sh
 . ./.travis/build.sh
