fe5a993b6d46ae23eb224015ba640035cd2ae2a5
sagudev <samo.golez@outlook.com>
2020-02-21 07:14:14 +0100

    r

--- .travis/emu/emu.sh
+++ .travis/emu/emu.sh
@@ -3,9 +3,17 @@ if ! ( echo $is | grep -q deploy ); then
 	sudo service docker stop
 	sudo dockerd --experimental &> /dev/null &
 	docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
-	docker run --rm --env-file <(env) arm64v8/ubuntu:16.04 env
-	docker run --rm arm32v7/ubuntu:16.04 uname -a
-	docker run --rm --platform 386 ubuntu:16.04 uname -a
+	case "$arch_build"
+		386*)
+			docker run --rm --env-file <(env) --platform 386 ubuntu:16.04 uname -a
+		;;
+		armhf*)
+			docker run --rm --env-file <(env) arm32v7/ubuntu:16.04 uname -a
+		;;
+		aarch*)
+			docker run --rm --env-file <(env) arm64v8/ubuntu:16.04 env	
+		;;
+	esac
 else
 	sudo apt-get update
 	sudo apt-get install -y -qq \
