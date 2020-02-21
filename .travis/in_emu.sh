69979d2fe5e6b0af4aec38332a697228df7cd52b
sagudev <samo.golez@outlook.com>
2020-02-21 09:32:51 +0100

    r

--- .travis/in_emu.sh
+++ .travis/in_emu.sh
@@ -20,6 +20,7 @@ cd "/home/travis/build/${TRAVIS_REPO_SLUG}"
 #echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 #echo "LANG=en_US.UTF-8" > /etc/locale.conf
 #locale-gen en_US.UTF-8
+export LANG=C.UTF-8
 . ./.travis/install_prerequisities.sh
 . ./.travis/setup_env.sh
 . ./.travis/build.sh
