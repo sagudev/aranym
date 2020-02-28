f22ccc6232c1500929aa4ce1468fce0750eb2675
sagudev <samo.golez@outlook.com>
2020-02-28 17:32:07 +0100

    r

--- .travis/emu.sh
+++ .travis/emu.sh
@@ -6,30 +6,31 @@ if ! [ "$deploy" = true ]; then
 	case "$arch_build" in
 		i386)
 			docker run --rm --env-file <(env) \
-				-v "/home/travis":"/home/travis" -w "/home/travis" \
+				-v "/home/travis":"/home/travis" -w "${PWD}" \
 				--platform 386 ubuntu:16.04 "${TRAVIS_BUILD_DIR}/.travis/in_emu.sh"
 		;;
 		armhf)
 			docker run --rm --env-file <(env) \
-				-v "/home/travis":"/home/travis" -w "/home/travis" \
+				-v "/home/travis":"/home/travis" -w "${PWD}" \
 				arm32v7/ubuntu:16.04 "${TRAVIS_BUILD_DIR}/.travis/in_emu.sh"
 		;;
 		aarch)
 			docker run --rm --env-file <(env) \
-				-v "/home/travis":"/home/travis" -w "/home/travis" \
+				-v "/home/travis":"/home/travis" -w "${PWD}" \
 				arm64v8/ubuntu:16.04 "${TRAVIS_BUILD_DIR}/.travis/in_emu.sh"	
 		;;
+		sudo chmod -Rf 777 /home/travis
 	esac
 else
 	case "$arch" in
 		armhf)
 			docker run --rm --env-file <(env) \
-				-v "/home/travis":"/home/travis" -w "/home/travis" \
+				-v "/home/travis":"/home/travis" -w "${PWD}" \
 				arm32v7/ubuntu:16.04 "${TRAVIS_BUILD_DIR}/.travis/in_emu.sh"
 		;;
 		aarch)
 			docker run --rm --env-file <(env) \
-				-v "/home/travis":"/home/travis" -w "/home/travis" \
+				-v "/home/travis":"/home/travis" -w "${PWD}" \
 				arm64v8/ubuntu:16.04 "${TRAVIS_BUILD_DIR}/.travis/in_emu.sh"	
 		;;
 	esac
